1e1c17a6c86de41394d29f49d7d93370e00e100b
Jerome Delos-Reyes <delosreyes.jerome@proton.me>
2024-06-28 21:58:02 -0400

    design login page with primeNG and tailwindcss

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { AuthenticateComponent } from './pages/auth/authenticate/authenticate.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', component: AuthenticateComponent },
+];
--- src/app/pages/auth/authenticate/authenticate.component.ts
+++ src/app/pages/auth/authenticate/authenticate.component.ts
@@ -1,18 +1,42 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../../../service/auth/auth.service';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { ButtonModule } from 'primeng/button';
+import { InputGroupModule } from 'primeng/inputgroup';
+import { InputGroupAddonModule } from 'primeng/inputgroupaddon';
+import { PasswordModule } from 'primeng/password';
+import { InputTextModule } from 'primeng/inputtext';
 
 @Component({
   selector: 'app-authenticate',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    ButtonModule,
+    InputGroupModule,
+    InputGroupAddonModule,
+    PasswordModule,
+    InputTextModule,
+  ],
   templateUrl: './authenticate.component.html',
   styleUrl: './authenticate.component.css',
 })
 export class AuthenticateComponent {
-  public header: string = 'Login';
+  public title: string = 'Authenticate';
+
+  public authRequest = new FormGroup({
+    username: new FormControl(''),
+    password: new FormControl(''),
+  });
 
   constructor(private authService: AuthService) {}
 
   public authenticate() {
+    this.authService.authenticate({
+      username: this.authRequest.value.username!,
+      password: this.authRequest.value.password!,
+    });
   }
 }
