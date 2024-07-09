22d49fdf6a5303d02132be47430ff026eaae96c1
Jerome Delos-Reyes <delosreyes.jerome@proton.me>
2024-07-09 00:29:50 -0400

    home page shows high level details of available movies

--- src/app/pages/visitor/home/home.component.ts
+++ src/app/pages/visitor/home/home.component.ts
@@ -2,11 +2,13 @@ import { Component, OnInit } from '@angular/core';
 import { VisitorService } from '../../../service/visitor/visitor.service';
 import { MovieResponse } from '../../../dto';
 import { Base64Pipe } from '../../../pipe/base64.pipe';
+import { LowerCasePipe } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [Base64Pipe],
+  imports: [Base64Pipe, LowerCasePipe, RouterLink],
   templateUrl: './home.component.html',
 })
 export class HomeComponent implements OnInit {
