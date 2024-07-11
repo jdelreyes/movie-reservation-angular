9f4b8e647f27c2d103e883fb3e58e45af36423ab
Jerome Delos-Reyes <delosreyes.jerome@proton.me>
2024-07-11 03:05:04 -0400

    add pagination for available movies

--- src/app/page/visitor/home/home.component.ts
+++ src/app/page/visitor/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { VisitorService } from '../../../service/visitor/visitor.service';
 import { MovieResponse } from '../../../dto';
 import { Base64Pipe } from '../../../pipe/base64.pipe';
@@ -8,8 +8,9 @@ import { ImageModule } from 'primeng/image';
 import { CardModule } from 'primeng/card';
 import { SkeletonModule } from 'primeng/skeleton';
 import { PaginatorModule } from 'primeng/paginator';
-import { PageEvent } from '../../event';
-import e from 'express';
+import { PageEvent } from '../../../event';
+import { finalize, Subject, takeUntil, tap } from 'rxjs';
+import { SourceTextModule } from 'node:vm';
 
 @Component({
   selector: 'app-home',
@@ -25,12 +26,16 @@ import e from 'express';
   ],
   templateUrl: './home.component.html',
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
   public title: string = 'Movies';
   public movieList: MovieResponse[] = [];
+
   public first: number = 0;
   public page: number = 0;
-  public rows: number = 2;
+  public size: number = 3;
+
+  public isLoading: boolean = true;
+  private unsubscribe$: Subject<unknown> = new Subject();
 
   constructor(private visitorService: VisitorService) {}
 
@@ -38,19 +43,42 @@ export class HomeComponent implements OnInit {
     this.getAvailableMovies();
   }
 
+  public ngOnDestroy(): void {
+    this.unsubscribe$.next(true);
+    this.unsubscribe$.complete();
+  }
+
   public onPageChange(event: PageEvent) {
     this.page = event.page;
-    this.rows = event.rows;
-    console.log(this.page);
-    console.log(this.rows);
+    this.size = event.rows;
+
+    console.log(this.page)
+    console.log(this.size)
+
     this.getAvailableMovies();
+    this.showUp();
   }
 
   private getAvailableMovies(): void {
     this.visitorService
-      .getAvailableMovies(this.page, this.rows)
-      .subscribe((movies: MovieResponse[]): void => {
-        this.movieList = movies;
-      });
+      .getAvailableMovies(this.page, this.size)
+      .pipe(
+        tap({
+          next: (movies: MovieResponse[]) => {
+            if (movies) {
+              this.movieList = movies;
+            }
+          },
+        }),
+        finalize(() => {
+          this.isLoading = false;
+        }),
+        takeUntil(this.unsubscribe$)
+      )
+      .subscribe();
+  }
+
+  private showUp(): void {
+    window.scroll(0, 0);
   }
 }
